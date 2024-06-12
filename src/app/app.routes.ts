51e7eed07da5738cb2a9e9df1f1ff893bcc71690
HarrisAva <140471903+HarrisAva@users.noreply.github.com>
2024-06-12 18:30:04 +0700

    modify navbar and app routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,38 +4,36 @@ import { AboutComponent } from './about/about.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { FrontLayoutComponent } from './layouts/front-layout/front-layout.component';
+import { ContactComponent } from './contact/contact.component';
 
 export const routes: Routes = [
   {
-path: '',
-component: FrontLayoutComponent
-  },
-
-
-
-  {
     path: '',
-    component: HomeComponent
-  },
-  {
-    path: 'about',
-    component: AboutComponent
-  },
+    component: FrontLayoutComponent, // parent component
+    children: [
 
-  {
-    path: 'contact',
-    component: AboutComponent
-  },
+        {
+          path: '',
+          component: HomeComponent // default child component
+        },
+        {
+          path: 'about',
+          component: AboutComponent
+        },
+        {
+          path: 'contact',
+          component: ContactComponent
+        },
+        {
+          path: 'login',
+          component: LoginComponent
+        },
+        {
+          path: 'register',
+          component: RegisterComponent
+        },
 
-  {
-    path: 'login',
-    component: LoginComponent
-  },
-
-  {
-    path: 'register',
-    component: RegisterComponent}
-
+    ]
   }
 
 ];
