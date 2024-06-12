35c1200f9da58d67d934dbc2ead9676eb3b4b2be
HarrisAva <140471903+HarrisAva@users.noreply.github.com>
2024-06-12 18:27:52 +0700

    complete login component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,69 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import {
+  ReactiveFormsModule,
+  FormsModule,
+  FormGroup,
+  FormBuilder,
+  Validators
+} from '@angular/forms'
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+
+  // FormGroup
+  loginForm!: FormGroup
+  // check if user submits form or not
+  submitted = false
+
+  // form data
+  userLogin = {
+    "email": "",
+    "password": ""
+  }
+
+  // Constructor
+  constructor(
+    private formBuilder: FormBuilder
+  ){
+  }
+
+  // ngOnInit
+  ngOnInit(){
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    })
+  }
+
+  submitLogin(){
+
+    this.submitted = true
+
+    // stop here if form is invalid
+    if(this.loginForm.invalid){
+      return
+    } else {
+      this.userLogin.email = this.loginForm.value.email
+      this.userLogin.password = this.loginForm.value.password
+
+      if(this.userLogin.email == "test@email.com" && this.userLogin.password == "password"){
+        alert("Login Success")
+      } else {
+        alert("Login Fail")
+      }
+
+    }
+  }
+
+  resetForm(){
+    this.submitted = false
+    this.loginForm.reset()
+  }
 
 }
